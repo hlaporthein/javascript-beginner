93d6d401224e8a86c2b72df838137ebc436129d1
hlaporthein <hlaporthein@gmail.com>
2019-01-21 16:00:23 +0630

    lessons

---
+++ external1.js
@@ -0,0 +1,10 @@
+function greeting() {
+	document.getElementById("container").innerHTML = "creating a function";
+}
+
+function say_name() {
+	document.getElementById("container").innerHTML = "Mg Mg";
+}
+
+
+document.getElementById("container").innerHTML = "Hello World";
